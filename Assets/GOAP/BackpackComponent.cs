538272bb88233598043ec381905c56ea7a49388f
FabioBrigidi <fabio.brigidi@gmail.com>
2018-05-15 11:30:39 +0200

    Started to setting up the GOAP example

---
+++ Assets/GOAP/BackpackComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/**
+ * Holds resources for the Agent.
+ */
+
+public class BackpackComponent : MonoBehaviour
+{
+
+    public GameObject tool;
+
+    public int numLogs;
+
+    public int numFirewood;
+
+    public int numOre;
+
+    public string toolType = "ToolAxe";
+
+}
---
+++ Assets/GOAP/Components/SupplyPileComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupplyPileComponent : MonoBehaviour
+{
+    public int numTools;    // for mining ore and chopping logs
+    public int numLogs;     // makes firewood
+    public int numFirewood; // what we want to make
+    public int numOre;      // makes tools
+}
